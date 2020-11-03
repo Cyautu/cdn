b5a8bba446b771dbc53056411eafbb9bf3fe0e33
yuu2b <1070177565@qq.com>
2020-11-03 09:19:58 +0800

    修改loader

--- js/loader.js
+++ js/loader.js
@@ -4,11 +4,15 @@
  */
 
 $('head').append(`<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/yuu2b/cdn/css/loader-ball.css">`)
-$('body').css('overflow','hidden').prepend('<div class="loader loader-ball is-active" shadow></div>');
-
+$('body')
+    .css('overflow','hidden')
+    .prepend('<div class="loader loader-ball is-active" shadow></div>')
+    .append(`<script>
 document.onreadystatechange = function () {
     if (document.readyState === 'complete'){
         $('.loader').remove();
         $('body').css('overflow','auto');
     }
 }
+</script>>`)
+
